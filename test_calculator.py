b845e31055323810a0d3f1ba52bfe5812ddabc3d
DenzMoses <125955008+DenzMoses@users.noreply.github.com>
2024-01-18 21:53:43 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -3,9 +3,9 @@
 from calculator import add, subtract
 
 def test_addition():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(2, 3) == 9
+    assert add(-1, 1) == 2
+    assert add(0, 0) == 1
 
 def test_subtraction():
     assert subtract(5, 3) == 2
